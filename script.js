3f5176d9629609a9c1eb079efc9e5dfeee081fab
Saiful Islam <saiful.bd.developer@gmail.com>
2023-11-24 21:15:51 +0600

    code refactor

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
-
-
-
- $(document).ready(function () {
-   var table = $("#example").DataTable({
-     paging: true,
-    
-     pageLength: 10,
-     ajax: function (data, callback, settings) {
-       console.log(data, "data, callback, settings");
-       $.ajax({
-         url: "http://13.232.225.92:8001/api/total-person",
-         type: "GET",
-         // contentType: "application/x-www-form-urlencoded", // Check if this is necessary
-         data: {
-           page: Math.floor(data.start / data.length) + 1,
-           size: data.length
-         },
-         success: function (apiResponse, textStatus, jQxhr) {
-           console.log(apiResponse, "apiResponse");
-           callback({
-             //  draw: data.draw, // Include draw property if necessary
-             data: apiResponse.data.results,
-             recordsTotal: apiResponse.data.count,
-             recordsFiltered: apiResponse.data.count
-           });
-         },
-         error: function (jqXhr, textStatus, errorThrown) {
-           // Handle errors
-         }
-       });
-     },
-     serverSide: true,
-     columns: [
-       { data: "id" },
-       { data: "name" },
-       { data: "email" },
-       { data: "phone_number" },
-       { data: "address" }
-     ]
-   });
- });
+$(document).ready(function () {
+  var table = $("#example").DataTable({
+    paging: true,
+    pageLength: 10,
+    serverSide: true,
+    ajax: {
+      url: "http://13.232.225.92:8001/api/total-person",
+      type: "GET",
+      data: function (data) {
+        var page = Math.floor(data?.start / data?.length) + 1;
+        var size = data?.length;
+        return { page: page, size: size };
+      },
+      dataSrc: function (apiResponse) {
+        var recordsTotal = apiResponse?.data?.count;
+        var recordsFiltered = recordsTotal;
+        $.extend(apiResponse, {
+          recordsTotal: recordsTotal,
+          recordsFiltered: recordsFiltered
+        });
+        return apiResponse?.data?.results;
+      },
+      error: function (jqXhr, textStatus, errorThrown) {
+        console.error("Error:", errorThrown);
+      }
+    },
+    columns: [
+      { data: "id" },
+      { data: "name" },
+      { data: "email" },
+      { data: "phone_number" },
+      { data: "address" }
+    ]
+  });
+});
