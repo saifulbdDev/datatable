cdacbfdec310c463f06e6acc48030fe85c733c01
Saiful Islam <saiful.bd.developer@gmail.com>
2023-11-24 20:40:37 +0600

    pagination integration done

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-$(document).ready(function () {
-  var table = $("#example").DataTable({
-    // scrollCollapse: true,
-    // scrollY: "800px",
-    ajax: {
-      url: "http://13.232.225.92:8001/api/total-person",
-      dataSrc: "data"
-    },
-    columns: [
-      { data: "id" },
-      { data: "name" },
-      { data: "email" },
-      { data: "phone_number"},
-      { data: "address" }
-    ]
-  });
-});
+
+
+
+ $(document).ready(function () {
+   var table = $("#example").DataTable({
+     paging: true,
+    
+     pageLength: 10,
+     ajax: function (data, callback, settings) {
+       console.log(data, "data, callback, settings");
+       $.ajax({
+         url: "http://13.232.225.92:8001/api/total-person",
+         type: "GET",
+         // contentType: "application/x-www-form-urlencoded", // Check if this is necessary
+         data: {
+           page: Math.floor(data.start / data.length) + 1,
+           size: data.length
+         },
+         success: function (apiResponse, textStatus, jQxhr) {
+           console.log(apiResponse, "apiResponse");
+           callback({
+             //  draw: data.draw, // Include draw property if necessary
+             data: apiResponse.data.results,
+             recordsTotal: apiResponse.data.count,
+             recordsFiltered: apiResponse.data.count
+           });
+         },
+         error: function (jqXhr, textStatus, errorThrown) {
+           // Handle errors
+         }
+       });
+     },
+     serverSide: true,
+     columns: [
+       { data: "id" },
+       { data: "name" },
+       { data: "email" },
+       { data: "phone_number" },
+       { data: "address" }
+     ]
+   });
+ });
